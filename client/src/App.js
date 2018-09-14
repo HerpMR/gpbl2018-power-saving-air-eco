ecc4a05ec9eb0cf1b448732ac4149836fb7d7d85
Duy Nguyen <truongduy.2611@gmail.com>
2018-09-15 06:16:27 +0700

    updated: new theme for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Slider from '@material-ui/lab/Slider';
 import './App.css';
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://192.168.43.185:3000');
+const socket = openSocket('http://192.168.0.101:3000');
 
 const subscribeToTimer = (callback) => {
   socket.on('timer', timestamp => callback(null, timestamp));
@@ -15,6 +16,35 @@ const subscribeToMotionSensor = (callback) => {
 const subscribeToSensor = (callback) => {
   socket.on('temperature_and_humidity', data => callback(null, data));
 }
+
+const Item = (props) => {
+  return (
+    <div className="Item">
+      <span> {props.text} </span>
+    </div>)
+}
+
+class SimpleSlider extends React.Component {
+  state = {
+    value: 50,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className="Simple-Slider">
+        <Slider value={value} aria-labelledby="label" onChange={this.handleChange} />
+      </div>
+    );
+  }
+}
+
 
 class App extends Component {
   constructor(props) {
@@ -56,14 +86,22 @@ class App extends Component {
 
   render() {
     const minusDegrees = Math.floor(this.state.people / 10);
+    const minusDegreesText = '- ' + this.state.people > 10 ? (minusDegrees > 5 ? 5 : minusDegrees) + '°C' : '';
     return (
       <div className="App">
-        <h3>Hello. It's {this.state.timestamp}.</h3>
-        <h3>There are {this.state.people} people.</h3>
-        <h3> 
-          Temperature: {this.state.temperature}°C 
-          {this.state.people > 10 && ` - ${minusDegrees > 5 ? 5: minusDegrees}°C.`}</h3>
-        <h3> Humidity: {this.state.humidity} %.</h3>
+        <div className="Holder">
+          <div className="Sub-Holder">
+            <Item text={`${this.state.timestamp}`}/>
+          </div>
+          <div className="Sub-Holder">
+            <Item text={`${this.state.people} people`}/>
+            <Item text={`${this.state.temperature}°C ${ minusDegreesText}`}/>
+            <Item text={`${this.state.humidity}%`}/>
+          </div>
+           <div className="Sub-Holder">
+            {/* <SimpleSlider /> */}
+           </div>
+        </div>
       </div>
     );
   }
